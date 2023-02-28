1791a3b4ca0858eb80ee9a8830c187f5bda4a832
eversmile12 <iwasvitto@gmail.com>
2023-02-28 15:27:37 -0700

    adds video support

--- frontend/pages/api/getNftsForCollection.js
+++ frontend/pages/api/getNftsForCollection.js
@@ -1,56 +1,54 @@
 import { Network, Alchemy } from "alchemy-sdk";
 
 export default async function handler(req, res) {
-	const { address, pageKey, pageSize, chain } = JSON.parse(
-		req.body
-	);
-	if (req.method !== "POST") {
-		res.status(405).send({ message: "Only POST requests allowed" });
-		return;
-	}
-	console.log(chain);
-	const settings = {
-		apiKey: process.env.ALCHEMY_API_KEY,
-		network: Network[chain],
-	};
-	const alchemy = new Alchemy(settings);
+  const { address, pageKey, pageSize, chain } = JSON.parse(req.body);
+  if (req.method !== "POST") {
+    res.status(405).send({ message: "Only POST requests allowed" });
+    return;
+  }
+  console.log(chain);
+  const settings = {
+    apiKey: process.env.ALCHEMY_API_KEY,
+    network: Network[chain],
+  };
+  const alchemy = new Alchemy(settings);
 
-	try {
-		const nfts = await alchemy.nft.getNftsForContract(address, {
-			pageKey: pageKey ? pageKey : null,
-			pageSize: pageSize ? pageSize : null,
-		});
-		const formattedNfts = nfts.nfts.map((nft) => {
-			const { contract, title, tokenType, tokenId, description, media } =
-				nft;
+  try {
+    const nfts = await alchemy.nft.getNftsForContract(address, {
+      pageKey: pageKey ? pageKey : null,
+      pageSize: pageSize ? pageSize : null,
+    });
+    const formattedNfts = nfts.nfts.map((nft) => {
+      const { contract, title, tokenType, tokenId, description, media } = nft;
 
-			return {
-				contract: contract.address,
-				symbol: contract.symbol,
-				media: media[0]?.gateway
-					? media[0]?.gateway
-					: "https://via.placeholder.com/500",
-				collectionName: contract.openSea?.collectionName,
-				verified: contract.openSea?.safelistRequestStatus,
-				tokenType,
-				tokenId,
-				title,
-				description,
-			};
-		});
-		 const filteredNfts = formattedNfts.filter(
+      return {
+        contract: contract.address,
+        symbol: contract.symbol,
+        media: media[0]?.gateway
+          ? media[0]?.gateway
+          : "https://via.placeholder.com/500",
+        collectionName: contract.openSea?.collectionName,
+        verified: contract.openSea?.safelistRequestStatus,
+        tokenType,
+        tokenId,
+        title,
+        description,
+        format: media[0]?.format ? media[0]?.format : "png",
+      };
+    });
+    const filteredNfts = formattedNfts.filter(
       (nft) => nft.title.length && nft.description.length && nft.media
     );
 
-		res.status(200).json({
-			nfts: filteredNfts,
-			pageKey: nfts.pageKey,
-		});
-		// the rest of your code
-	} catch (e) {
-		console.warn(e);
-		res.status(500).send({
-			message: "something went wrong, check the log in your terminal",
-		});
-	}
+    res.status(200).json({
+      nfts: filteredNfts,
+      pageKey: nfts.pageKey,
+    });
+    // the rest of your code
+  } catch (e) {
+    console.warn(e);
+    res.status(500).send({
+      message: "something went wrong, check the log in your terminal",
+    });
+  }
 }
